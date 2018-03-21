99d976ab15ba05eedd692c502e08af08dd75ccc5
wolfm2@newschool.edu <wolfm2@newschool.edu>
2018-03-21 19:35:54 -0400

    modify to load on scroll

--- js/code.js
+++ js/code.js
@@ -3,22 +3,104 @@ Locates all student work objects in global namespace.
 Inits page.
 */
 
-// set background
-function setBg () {
-  img = new Image();
-  img.src = "assets/background.png";
-  img.style.opacity = "0.3";
-  document.body.appendChild(img);
+// test if in viewport
+function isOnScreen(element) {
+
+    var win = $(window);
+
+    var viewport = {
+        top: win.scrollTop(),
+        left: win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = element.offset();
+    bounds.right = bounds.left + element.outerWidth();
+    bounds.bottom = bounds.top + element.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
 }
+
+// a pythonesque format method
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        var args;
+        args = arguments;
+        if (args.length === 1 && args[0] !== null && typeof args[0] === 'object') {
+            args = args[0];
+        }
+        return this.replace(/{([^}]*)}/g, function(match, key) {
+            return (typeof args[key] !== "undefined" ? args[key] : match);
+        });
+    };
+}
+
+entry = `
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+          <div class="card h-100">
+            <a href="#"><img class="card-img-top" src={0} alt=""></a>
+            <div class="card-body">
+              <h4 class="card-title">
+                <a href="#">{1}</a>
+              </h4>
+              <p class="card-text">{2}</p>
+            </div>
+          </div>
+        </div>
+`;
+
+foot = '<span id="foot" class="col-lg-4 col-sm-6 portfolio-item">.</span>';
+
+curEntry = 0; // current entry
+
+// set background
+function setBg() {
+    img = new Image();
+    img.src = "assets/background.png";
+    img.style.opacity = "0.3";
+    document.body.appendChild(img);
+}
+
+function showEntry() {
+    while (isOnScreen($("#foot")) && curEntry < entryData.length) {
+        data = window[entryData[curEntry]];
+        html = entry.format(data.photo, data.name, data.bio);
+        $(html).appendTo(".row").show('fast');
+        $(".row > div").css("opacity", "1");
+        $("#foot").remove();
+        $(foot).appendTo(".row");
+        curEntry++;
+        console.log(curEntry);
+        console.log(isOnScreen($("#foot")));
+    }
+}
+
+function entriesLoaded() {
+    //$("BODY").click(function() {
+    //  addPerson();
+    //});
+    $(foot).appendTo(".row")
+    showEntry();
+    $(document).scroll(function() {
+        showEntry();
+    });
+}
+
+entryData = []; // sort it however
 
 // main
 $(document).ready(function() {
-  setBg();
-  for (var item in window) {
-    
-    if ( /^N([0-9]{8})$/.test(item) ) { // find entries
-      console.log("Found: " + item);
-      $('#test').html(window[item].email);
+    // setBg();
+
+    for (var item in window) {
+        // find entries in global namespace
+        if (/^N([0-9]{8})$/.test(item)) {
+            console.log("Found: " + item);
+            entryData.push(item);
+        }
     }
-  }
+
+    $("#people").load("assets/startbootstrap-3-col-portfolio-gh-pages/index-ppl.html", entriesLoaded);
 });
+
--- js/data.js
+++ js/data.js
@@ -2,9 +2,207 @@ N00334855={
 email:'hillar', 
 name:'Aaron Hill', 
 role:'faculty', 
-photo:'aaron.png', 
+photo:'assets/pic.jpg', 
 site:'www.aaronhill.io',
-bio:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar vel urna vel maximus. Nulla porttitor nec nunc eu suscipit. Curabitur molestie, metus nec aliquam placerat, lectus neque consectetur dolor, at facilisis tellus mauris nec felis. In eu volutpat diam. Pellentesque fermentum purus sodales, tincidunt lorem nec, vehicula ante. Nam volutpat felis cursus iaculis lobortis. In nec arcu eu augue porttitor vehicula vel et orci. Aliquam erat volutpat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut velit volutpat sapien lobortis pellentesque. Mauris dapibus mollis ornare.',
+bio:'Aaron Hill is a data scientist, educator, and researcher. He is the director of the MS Data Visualization program in the School of Art, Media and Technology at Parsons The New School for Design. He holds a master’s degree in urban policy. ',
+projects:[
+    {
+      heading:'Changes in Health, Education, and Income Inequality in Sub-Saharan Africa',
+      subHead:'The numerical value for each inequality is the Atkinson Measure as reported by the UNDP in the 2015 Human Development Report [1]. The Atkinson Measure is a measure of inequality that varies between 0 and 1 -- or 0% and 100%, equivalently -- with 0 indicating no inequality and 1 indicating complete inequality. The visualization is a synthesized re-rendering of three charts found in Chapter 11 of Income Inequality Trends in Sub-Saharan Africa [2], which plot the same data. The intended goal is to emphasize and clarify the connection between these three charts, providing a user-friendly way of tracking both the absolute and relative change undergone by each individual country betwen 2010 and 2014 in all three areas of inequality.',
+      desc:'This visualization investigates the ways in which education inequality, health inequality, and income inequality changed from 2010 to 2014 in the different countries of Sub-Saharan Africa.',
+      photoUrl:'rikwork.png',
+      addlPhotos:['rikwork2.png', 'rikwork3.png'],
+      workUrl:'https://rikghosh.github.io/ms-1/tools/index.html',
+      month:'December',
+      year:2017,
+      metadata:['united nations', 'un', 'education', 'health', 'income inequality']
+    }
+  ]
+}
+
+N00334851={
+email:'hillar', 
+name:'Aaron Hill', 
+role:'faculty', 
+photo:'assets/pic.jpg', 
+site:'www.aaronhill.io',
+bio:'Aaron Hill is a data scientist, educator, and researcher. He is the director of the MS Data Visualization program in the School of Art, Media and Technology at Parsons The New School for Design. He holds a master’s degree in urban policy. ',
+projects:[
+    {
+      heading:'Changes in Health, Education, and Income Inequality in Sub-Saharan Africa',
+      subHead:'The numerical value for each inequality is the Atkinson Measure as reported by the UNDP in the 2015 Human Development Report [1]. The Atkinson Measure is a measure of inequality that varies between 0 and 1 -- or 0% and 100%, equivalently -- with 0 indicating no inequality and 1 indicating complete inequality. The visualization is a synthesized re-rendering of three charts found in Chapter 11 of Income Inequality Trends in Sub-Saharan Africa [2], which plot the same data. The intended goal is to emphasize and clarify the connection between these three charts, providing a user-friendly way of tracking both the absolute and relative change undergone by each individual country betwen 2010 and 2014 in all three areas of inequality.',
+      desc:'This visualization investigates the ways in which education inequality, health inequality, and income inequality changed from 2010 to 2014 in the different countries of Sub-Saharan Africa.',
+      photoUrl:'rikwork.png',
+      addlPhotos:['rikwork2.png', 'rikwork3.png'],
+      workUrl:'https://rikghosh.github.io/ms-1/tools/index.html',
+      month:'December',
+      year:2017,
+      metadata:['united nations', 'un', 'education', 'health', 'income inequality']
+    }
+  ]
+}
+
+N00334852={
+email:'hillar', 
+name:'Aaron Hill', 
+role:'faculty', 
+photo:'assets/pic.jpg', 
+site:'www.aaronhill.io',
+bio:'Aaron Hill is a data scientist, educator, and researcher. He is the director of the MS Data Visualization program in the School of Art, Media and Technology at Parsons The New School for Design. He holds a master’s degree in urban policy. ',
+projects:[
+    {
+      heading:'Changes in Health, Education, and Income Inequality in Sub-Saharan Africa',
+      subHead:'The numerical value for each inequality is the Atkinson Measure as reported by the UNDP in the 2015 Human Development Report [1]. The Atkinson Measure is a measure of inequality that varies between 0 and 1 -- or 0% and 100%, equivalently -- with 0 indicating no inequality and 1 indicating complete inequality. The visualization is a synthesized re-rendering of three charts found in Chapter 11 of Income Inequality Trends in Sub-Saharan Africa [2], which plot the same data. The intended goal is to emphasize and clarify the connection between these three charts, providing a user-friendly way of tracking both the absolute and relative change undergone by each individual country betwen 2010 and 2014 in all three areas of inequality.',
+      desc:'This visualization investigates the ways in which education inequality, health inequality, and income inequality changed from 2010 to 2014 in the different countries of Sub-Saharan Africa.',
+      photoUrl:'rikwork.png',
+      addlPhotos:['rikwork2.png', 'rikwork3.png'],
+      workUrl:'https://rikghosh.github.io/ms-1/tools/index.html',
+      month:'December',
+      year:2017,
+      metadata:['united nations', 'un', 'education', 'health', 'income inequality']
+    }
+  ]
+}
+
+N00334853={
+email:'hillar', 
+name:'Aaron Hill', 
+role:'faculty', 
+photo:'assets/pic.jpg', 
+site:'www.aaronhill.io',
+bio:'Aaron Hill is a data scientist, educator, and researcher. He is the director of the MS Data Visualization program in the School of Art, Media and Technology at Parsons The New School for Design. He holds a master’s degree in urban policy. ',
+projects:[
+    {
+      heading:'Changes in Health, Education, and Income Inequality in Sub-Saharan Africa',
+      subHead:'The numerical value for each inequality is the Atkinson Measure as reported by the UNDP in the 2015 Human Development Report [1]. The Atkinson Measure is a measure of inequality that varies between 0 and 1 -- or 0% and 100%, equivalently -- with 0 indicating no inequality and 1 indicating complete inequality. The visualization is a synthesized re-rendering of three charts found in Chapter 11 of Income Inequality Trends in Sub-Saharan Africa [2], which plot the same data. The intended goal is to emphasize and clarify the connection between these three charts, providing a user-friendly way of tracking both the absolute and relative change undergone by each individual country betwen 2010 and 2014 in all three areas of inequality.',
+      desc:'This visualization investigates the ways in which education inequality, health inequality, and income inequality changed from 2010 to 2014 in the different countries of Sub-Saharan Africa.',
+      photoUrl:'rikwork.png',
+      addlPhotos:['rikwork2.png', 'rikwork3.png'],
+      workUrl:'https://rikghosh.github.io/ms-1/tools/index.html',
+      month:'December',
+      year:2017,
+      metadata:['united nations', 'un', 'education', 'health', 'income inequality']
+    }
+  ]
+}
+
+N00334854={
+email:'hillar', 
+name:'Aaron Hill', 
+role:'faculty', 
+photo:'assets/pic.jpg', 
+site:'www.aaronhill.io',
+bio:'Aaron Hill is a data scientist, educator, and researcher. He is the director of the MS Data Visualization program in the School of Art, Media and Technology at Parsons The New School for Design. He holds a master’s degree in urban policy. ',
+projects:[
+    {
+      heading:'Changes in Health, Education, and Income Inequality in Sub-Saharan Africa',
+      subHead:'The numerical value for each inequality is the Atkinson Measure as reported by the UNDP in the 2015 Human Development Report [1]. The Atkinson Measure is a measure of inequality that varies between 0 and 1 -- or 0% and 100%, equivalently -- with 0 indicating no inequality and 1 indicating complete inequality. The visualization is a synthesized re-rendering of three charts found in Chapter 11 of Income Inequality Trends in Sub-Saharan Africa [2], which plot the same data. The intended goal is to emphasize and clarify the connection between these three charts, providing a user-friendly way of tracking both the absolute and relative change undergone by each individual country betwen 2010 and 2014 in all three areas of inequality.',
+      desc:'This visualization investigates the ways in which education inequality, health inequality, and income inequality changed from 2010 to 2014 in the different countries of Sub-Saharan Africa.',
+      photoUrl:'rikwork.png',
+      addlPhotos:['rikwork2.png', 'rikwork3.png'],
+      workUrl:'https://rikghosh.github.io/ms-1/tools/index.html',
+      month:'December',
+      year:2017,
+      metadata:['united nations', 'un', 'education', 'health', 'income inequality']
+    }
+  ]
+}
+
+N00334856={
+email:'hillar', 
+name:'Aaron Hill', 
+role:'faculty', 
+photo:'assets/pic.jpg', 
+site:'www.aaronhill.io',
+bio:'Aaron Hill is a data scientist, educator, and researcher. He is the director of the MS Data Visualization program in the School of Art, Media and Technology at Parsons The New School for Design. He holds a master’s degree in urban policy. ',
+projects:[
+    {
+      heading:'Changes in Health, Education, and Income Inequality in Sub-Saharan Africa',
+      subHead:'The numerical value for each inequality is the Atkinson Measure as reported by the UNDP in the 2015 Human Development Report [1]. The Atkinson Measure is a measure of inequality that varies between 0 and 1 -- or 0% and 100%, equivalently -- with 0 indicating no inequality and 1 indicating complete inequality. The visualization is a synthesized re-rendering of three charts found in Chapter 11 of Income Inequality Trends in Sub-Saharan Africa [2], which plot the same data. The intended goal is to emphasize and clarify the connection between these three charts, providing a user-friendly way of tracking both the absolute and relative change undergone by each individual country betwen 2010 and 2014 in all three areas of inequality.',
+      desc:'This visualization investigates the ways in which education inequality, health inequality, and income inequality changed from 2010 to 2014 in the different countries of Sub-Saharan Africa.',
+      photoUrl:'rikwork.png',
+      addlPhotos:['rikwork2.png', 'rikwork3.png'],
+      workUrl:'https://rikghosh.github.io/ms-1/tools/index.html',
+      month:'December',
+      year:2017,
+      metadata:['united nations', 'un', 'education', 'health', 'income inequality']
+    }
+  ]
+}
+
+N00334857={
+email:'hillar', 
+name:'Aaron Hill', 
+role:'faculty', 
+photo:'assets/pic.jpg', 
+site:'www.aaronhill.io',
+bio:'Aaron Hill is a data scientist, educator, and researcher. He is the director of the MS Data Visualization program in the School of Art, Media and Technology at Parsons The New School for Design. He holds a master’s degree in urban policy. ',
+projects:[
+    {
+      heading:'Changes in Health, Education, and Income Inequality in Sub-Saharan Africa',
+      subHead:'The numerical value for each inequality is the Atkinson Measure as reported by the UNDP in the 2015 Human Development Report [1]. The Atkinson Measure is a measure of inequality that varies between 0 and 1 -- or 0% and 100%, equivalently -- with 0 indicating no inequality and 1 indicating complete inequality. The visualization is a synthesized re-rendering of three charts found in Chapter 11 of Income Inequality Trends in Sub-Saharan Africa [2], which plot the same data. The intended goal is to emphasize and clarify the connection between these three charts, providing a user-friendly way of tracking both the absolute and relative change undergone by each individual country betwen 2010 and 2014 in all three areas of inequality.',
+      desc:'This visualization investigates the ways in which education inequality, health inequality, and income inequality changed from 2010 to 2014 in the different countries of Sub-Saharan Africa.',
+      photoUrl:'rikwork.png',
+      addlPhotos:['rikwork2.png', 'rikwork3.png'],
+      workUrl:'https://rikghosh.github.io/ms-1/tools/index.html',
+      month:'December',
+      year:2017,
+      metadata:['united nations', 'un', 'education', 'health', 'income inequality']
+    }
+  ]
+}
+
+N00334858={
+email:'hillar', 
+name:'Aaron Hill', 
+role:'faculty', 
+photo:'assets/pic.jpg', 
+site:'www.aaronhill.io',
+bio:'Aaron Hill is a data scientist, educator, and researcher. He is the director of the MS Data Visualization program in the School of Art, Media and Technology at Parsons The New School for Design. He holds a master’s degree in urban policy. ',
+projects:[
+    {
+      heading:'Changes in Health, Education, and Income Inequality in Sub-Saharan Africa',
+      subHead:'The numerical value for each inequality is the Atkinson Measure as reported by the UNDP in the 2015 Human Development Report [1]. The Atkinson Measure is a measure of inequality that varies between 0 and 1 -- or 0% and 100%, equivalently -- with 0 indicating no inequality and 1 indicating complete inequality. The visualization is a synthesized re-rendering of three charts found in Chapter 11 of Income Inequality Trends in Sub-Saharan Africa [2], which plot the same data. The intended goal is to emphasize and clarify the connection between these three charts, providing a user-friendly way of tracking both the absolute and relative change undergone by each individual country betwen 2010 and 2014 in all three areas of inequality.',
+      desc:'This visualization investigates the ways in which education inequality, health inequality, and income inequality changed from 2010 to 2014 in the different countries of Sub-Saharan Africa.',
+      photoUrl:'rikwork.png',
+      addlPhotos:['rikwork2.png', 'rikwork3.png'],
+      workUrl:'https://rikghosh.github.io/ms-1/tools/index.html',
+      month:'December',
+      year:2017,
+      metadata:['united nations', 'un', 'education', 'health', 'income inequality']
+    }
+  ]
+}
+
+N00334859={
+email:'hillar', 
+name:'Aaron Hill', 
+role:'faculty', 
+photo:'assets/pic.jpg', 
+site:'www.aaronhill.io',
+bio:'Aaron Hill is a data scientist, educator, and researcher. He is the director of the MS Data Visualization program in the School of Art, Media and Technology at Parsons The New School for Design. He holds a master’s degree in urban policy. ',
+projects:[
+    {
+      heading:'Changes in Health, Education, and Income Inequality in Sub-Saharan Africa',
+      subHead:'The numerical value for each inequality is the Atkinson Measure as reported by the UNDP in the 2015 Human Development Report [1]. The Atkinson Measure is a measure of inequality that varies between 0 and 1 -- or 0% and 100%, equivalently -- with 0 indicating no inequality and 1 indicating complete inequality. The visualization is a synthesized re-rendering of three charts found in Chapter 11 of Income Inequality Trends in Sub-Saharan Africa [2], which plot the same data. The intended goal is to emphasize and clarify the connection between these three charts, providing a user-friendly way of tracking both the absolute and relative change undergone by each individual country betwen 2010 and 2014 in all three areas of inequality.',
+      desc:'This visualization investigates the ways in which education inequality, health inequality, and income inequality changed from 2010 to 2014 in the different countries of Sub-Saharan Africa.',
+      photoUrl:'rikwork.png',
+      addlPhotos:['rikwork2.png', 'rikwork3.png'],
+      workUrl:'https://rikghosh.github.io/ms-1/tools/index.html',
+      month:'December',
+      year:2017,
+      metadata:['united nations', 'un', 'education', 'health', 'income inequality']
+    }
+  ]
+}
+
+N00334850={
+email:'hillar', 
+name:'Aaron Hill', 
+role:'faculty', 
+photo:'assets/pic.jpg', 
+site:'www.aaronhill.io',
+bio:'Aaron Hill is a data scientist, educator, and researcher. He is the director of the MS Data Visualization program in the School of Art, Media and Technology at Parsons The New School for Design. He holds a master’s degree in urban policy. ',
 projects:[
     {
       heading:'Changes in Health, Education, and Income Inequality in Sub-Saharan Africa',
